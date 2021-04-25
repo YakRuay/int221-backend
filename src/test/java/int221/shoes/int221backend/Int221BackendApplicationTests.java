90bfeb64e1598c3e29d384700fe78d3b3bff2b58
RewBugLag <rewrew2000@gmail.com>
2021-04-26 00:41:17 +0700

    Initial commit

---
+++ src/test/java/int221/shoes/int221backend/Int221BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package int221.shoes.int221backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Int221BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
